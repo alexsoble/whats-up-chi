13bd808a51ece750c2597a9421533a77b0ca7ac6
alexsoble <asoble@gmail.com>
2015-09-27 11:12:53 -0700

    Structure the js a little

---
+++ js/neighborhoods.js
@@ -0,0 +1,27 @@
+(function(root) {
+
+  var Neighborhoods = {
+    'lincoln-park': [41.9200, -87.6500],
+    'little-village': [41.8500, -87.7100],
+    'logan-square': [41.9283, -87.7067],
+    'loop': [41.881832, -87.623177],
+    'downtown': [41.881832, -87.623177],
+    'lakeview': [41.94352, -87.654102],
+    'river-north': [41.9, -87.63],
+    'albany-park': [41.97, -87.72],
+    'little-italy': [41.871, -87.659],
+    'wicker-park': [41.9075, -87.676944],
+    'uptown': [41.97, -87.66],
+    'chatham': [41.74,-87.611667],
+    'hyde-park': [41.8, -87.59],
+    'bronzeville': [41.83, -87.62],
+    'west-loop': [41.87, -87.67],
+    'humboldt-park': [41.88, -87.7],
+    'irving-park': [41.95, -87.73],
+    'wrigleyville': [41.95, -87.655],
+    'rogers-park': [42.01, -87.67]
+  };
+
+  root.Neighborhoods = Neighborhoods;
+
+})(window)
