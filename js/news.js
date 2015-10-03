1948d8ec689e9522b7dd86a4a76b62c91f97c703
alexsoble <asoble@gmail.com>
2015-10-03 08:55:39 -0700

    Throttle marker placement on map

--- js/news.js
+++ js/news.js
@@ -18,16 +18,22 @@
     var items = data.query.results.rss.channel.item;
     for (i = 0; i < items.length; i ++) {
       var item = items[i];
+      self.delayedAddMarker(i, item, markers);
+    };
+    var controller = new ArticlesController(map, markers);
+    controller.show();
+  };
+
+  News.prototype.delayedAddMarker = function (i, item, markers) {
+    setTimeout(function() {
       var article = new Article(item.link, item.title)
       article.toMarker().then(function(response) {
         markers.addLayer(response);
       }, function(error) {
         reject(Error("Failed!" + error));
       });
-    };
-    var controller = new ArticlesController(map, markers);
-    controller.show();
-  };
+    }, i * 500);
+  }
 
   root.News = News;
 
