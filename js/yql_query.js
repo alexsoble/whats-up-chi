c448cb444e0e95447d72119a07bb2c61d335b453
Alex <asoble@gmail.com>
2016-03-25 06:06:25 -0500

    Request our Yahoo queries via HTTPS

--- js/yql_query.js
+++ js/yql_query.js
@@ -9,13 +9,13 @@
   }
 
   YqlArticleQuery.prototype.fullQuery = function () {
-    return  'http://query.yahooapis.com/v1/public/yql?q=' +
+    return  'https://query.yahooapis.com/v1/public/yql?q=' +
             encodeURIComponent('select * from html where url="' + this.url + '"') +
             'and xpath="//article[contains(@class,\'story\')]//text()"&format=json&callback=?';
   };
 
   YqlRssQuery.prototype.fullQuery = function () {
-    return  'http://query.yahooapis.com/v1/public/yql?q=' +
+    return  'https://query.yahooapis.com/v1/public/yql?q=' +
             encodeURIComponent('select * from xml where url="' + this.url + '"') +
             '&format=json&callback=?';
   };
